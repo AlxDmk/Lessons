a0940061e1e636202f08687e249f56016e977107
AlxDmk <alxdmk@gmail.com>
2021-07-20 14:44:29 +0300

    Домашнее задание к уроку 1 C#

---
+++ Lessons/Lesson1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Ваше имя:");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Привет, {userName}! Сегодня: {DateTime.Today.ToString("D")}");
+        }
+    }
+}
