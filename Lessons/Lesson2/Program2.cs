ce72e2ffef2502acb8e10a2eef7ba0347fdb8693
AlxDmk <alxdmk@gmail.com>
2021-07-26 01:44:17 +0300

    Добавлено Задание №1 Lesson2

--- Lessons/Lesson2/Program2.cs
+++ Lessons/Lesson2/Program2.cs
@@ -6,8 +6,14 @@ namespace Lessons
 
     {
         static void Main(string[] args)
-        {
-                    
+        {  
+            // Запрос минимальной и максимальной температуры и вывод срденей за сутки
+            Console.WriteLine("Введите минимальную температуру за сутки");
+            int minTemp = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите максимальную температуру за сутки");
+            int maxTemp = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Средня температура за сутки составляет: {(maxTemp+minTemp)/2}");
+
         }
     }
 }
