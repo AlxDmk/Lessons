a4111ca51d1c7cdc2723a663645fbc99935facf0
AlxDmk <alxdmk@gmail.com>
2021-07-25 15:21:09 +0300

    Создал папку для ДЗ второго урока

--- Lessons/Lesson1/Program.cs
+++ Lessons/Lesson1/Program.cs
@@ -4,11 +4,11 @@ namespace Lessons
 {
     class Program
     {
-        static void Main(string[] args)
+        /* static void Main(string[] args)
         {
             Console.WriteLine("Введите Ваше имя:");
             string userName = Console.ReadLine();
             Console.WriteLine($"Привет, {userName}! Сегодня: {DateTime.Today.ToString("D")}");
-        }
+        }*/
     }
 }
---
+++ Lessons/Lesson2/Program2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lessons
+{
+    class Program2
+
+    {
+        static void Main(string[] args)
+        {
+                    
+        }
+    }
+}
