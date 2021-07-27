6eb24f6b4788a400816dba5df3a55be889ed0835
AlxDmk <alxdmk@gmail.com>
2021-07-27 13:16:29 +0300

    Добавлено задание 3 к Lesson3

---
+++ Lessons/Lesson3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lessons
+{
+    class Program3
+    {
+        static void Main(string[] args)
+        {
+           
+            // 3. Написать слово в обратном порядке
+            char[] hellо = "hello".ToCharArray();
+
+            for (int i = hellо.Length - 1; i >= 0; --i)
+            {
+                Console.Write(hellо[i]);
+            }
+
+
+        }
+    }
+}
